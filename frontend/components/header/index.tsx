2ac3b15843fa2cfc27c06e7295348079aadaf52f
Stephen <thuqpalmer@gmail.com>
2023-02-15 17:53:51 +0000

    [add] links

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -5,29 +5,23 @@ import Logo from "../logo/Logo";
 const Header = () => {
   return (
     <header className=" sticky top-0 h-20 w-full bg-[#BAE6FD] ">
-      <div className="mx-auto flex h-full px-4 md:px-10 lg:px-2 max-w-6xl items-center justify-between">
+      <div className="mx-auto flex h-full max-w-6xl items-center justify-between px-4 md:px-10 lg:px-2">
         <Link href="/" className=" h-full">
           <Logo />
         </Link>
-        {/* <nav className="flex gap-6">
-          {ROUTES.map((route) => {
-            const { link, name } = route;
-            return (
-              <Link key={name} href={link} className="capitalize">
-                {name}
-              </Link>
-            );
-          })}
-        </nav> */}
         <div className="flex gap-8">
-          <Button
-            radius="10px"
-            label="Sign in"
-            color="#2221"
-            textColor="#222"
-          />
+          <Link href="/auth/login">
+            <Button
+              radius="10px"
+              label="Sign in"
+              color="#2221"
+              textColor="#222"
+            />
+          </Link>
           <div className="hidden md:block">
-            <Button radius="10px" label="Sign up" color="#7E22CE" />
+            <Link href="/auth/signup">
+              <Button radius="10px" label="Sign up" color="#7E22CE" />
+            </Link>
           </div>
         </div>
       </div>
--- frontend/pages/auth/login.tsx
+++ frontend/pages/auth/login.tsx
@@ -2,11 +2,18 @@ import Button from "@/components/button/Button";
 import Input from "@/components/form/Input";
 import Logo from "@/components/logo/Logo";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
-const login = () => {
+const Login = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log("submitted");
+  };
   return (
     <>
-      {/* <MainLayout> */}
       <div className="flex h-screen">
         <div className=" w-[50%] ">
           <div className="absolute left-10">
@@ -23,38 +30,37 @@ const login = () => {
           </div>
         </div>
         <section className="flex flex-1 flex-col bg-[#2221]">
-          <div className="justify-end flex  h-20 items-center pr-20">
-            <Button radius="10px" label="Sign up" color="#7E22CE" />
+          <div className="flex h-20  items-center justify-end pr-20">
+            <Link href="/auth/signup">
+              <Button radius="10px" label="Sign up" color="#7E22CE" />
+            </Link>
           </div>
           <div className="flex flex-1 flex-col items-center justify-center ">
             <div className="flex  w-[30em]  flex-col rounded-md border-gray-700 p-6">
               <div className="">
-                {/* <div className="mb-1">
-                  <Image
-                    src="/assets/logo/logo_new.png"
-                    width={300}
-                    height={400}
-                    alt="logo"
-                    className="mx-20 h-12 w-[180px] object-cover"
-                  />
-                </div> */}
                 <h2 className="pb-4 text-2xl font-medium capitalize text-purple-700">
                   login to continue
                 </h2>
-                {/* <h2></h2> */}
               </div>
-              <form className="flex flex-col gap-2" action="">
+              <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
                 <Input placeholder="Email" label="Email" type="email" />
                 <Input
                   placeholder="password"
                   label="password"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                 />
 
                 <div className="mb-1" />
                 <div className="flex gap-2">
-                    <input type="checkbox" name="" id="" />
-                    <label htmlFor="">View Password</label>
+                  <input
+                    type="checkbox"
+                    name="password"
+                    onChange={() => {
+                      setShowPassword((prev) => !prev);
+                    }}
+                    id=""
+                  />
+                  <label htmlFor="password">View Password</label>
                 </div>
                 <Button
                   label="LOGIN"
@@ -63,18 +69,13 @@ const login = () => {
                   // textColor=""
                   width="100%"
                 />
-                {/* <div className=" my-2  text-center text-lg">
-                <p className="text-gray-600">Don&apos;t have an account?</p>
-                <Link href="/auth/signup">Sign up</Link>
-              </div> */}
               </form>
             </div>
           </div>
         </section>
       </div>
-      {/* </MainLayout> */}
     </>
   );
 };
 
-export default login;
+export default Login;
